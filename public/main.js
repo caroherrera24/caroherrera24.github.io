25ac95d102d2dec7afe601eda283bb2a492c1e2c
Carolina Herrera <ch.caro.her@gmail.com>
2025-01-03 04:08:59 -0500

    add media queries and hamburger menu

--- public/main.js
+++ public/main.js
@@ -1,8 +1,42 @@
 let  activeElement = "";
-const items = document.querySelectorAll(".nav-item")
+const items = document.querySelectorAll(".nav-item");
 items.forEach((element) =>
     element.addEventListener("click", () => {
         items.forEach((element) => element.className = "");
         element.className = "active";
     }
 ));
+
+const hamburger = document.querySelector("#hamburger");
+const nav = document.querySelector(".left-section");
+const closeIcon = document.querySelector("#close-icon");
+const openIcon = document.querySelector("#open-icon");
+// const ariaExpanded = nav.getAttribute("aria-expanded");
+let open = false;
+hamburger.addEventListener("click", () => {
+    if (!open) {
+        nav.classList.remove("hidden");
+        closeIcon.classList.remove("hidden");
+        openIcon.classList.add("hidden");
+        open = true;
+
+    } else {
+        nav.classList.add("hidden");
+        closeIcon.classList.add("hidden");
+        openIcon.classList.remove("hidden");
+        open = false;
+    }
+
+    if (open) {
+        items.forEach((element) =>
+            element.addEventListener("click", () => {
+                nav.classList.add("hidden");
+                closeIcon.classList.add("hidden");
+                openIcon.classList.remove("hidden");
+                open = false;
+            }
+        ));
+    }
+
+    hamburger.setAttribute("aria-expanded", open);
+})
